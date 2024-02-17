2adc1ad2c042b1103cce836c53c174e98f0f7bb0
GeorgeSolorio <Georgemsolorio@gmail.com>
2024-02-17 11:21:52 -0800

    Create the `FeedItem` model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by George Solorio on 2/17/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
