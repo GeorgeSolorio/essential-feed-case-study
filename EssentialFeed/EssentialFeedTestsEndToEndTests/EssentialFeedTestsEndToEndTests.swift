ec36b2f83c8a4d7d8fddf40840a203a48af2ad30
GeorgeSolorio <Georgemsolorio@gmail.com>
2024-03-01 17:34:14 -0800

    Extend timeout

--- EssentialFeed/EssentialFeedTestsEndToEndTests/EssentialFeedTestsEndToEndTests.swift
+++ EssentialFeed/EssentialFeedTestsEndToEndTests/EssentialFeedTestsEndToEndTests.swift
@@ -48,7 +48,7 @@ final class EssentialFeedTestsEndToEndTests: XCTestCase {
             exp.fulfill()
         }
         
-        wait(for: [exp], timeout: 5.0)
+        wait(for: [exp], timeout: 10.0)
         return receivedResult
     }
     
