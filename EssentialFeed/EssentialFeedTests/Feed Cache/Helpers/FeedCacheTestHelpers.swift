9aa35272aa4bf7c3b8556298623662141b75cee2
GeorgeSolorio <Georgemsolorio@gmail.com>
2024-03-18 00:00:12 -0700

    Seperate `Date` extension helpers into distinct context to clarify their scope

--- EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -23,15 +23,17 @@ extension Date {
         return 7
     }
     
-    func adding(days: Int) -> Date {
+    private func adding(days: Int) -> Date {
         return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
-    }
-    
-    func adding(seconds: TimeInterval) -> Date {
-        return self + seconds
     }
     
     func minusFeedCacheMaxAge() -> Date {
         return adding(days: -feedCacheMaxAgeInDays)
+    }
+}
+
+extension Date {
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
     }
 }
