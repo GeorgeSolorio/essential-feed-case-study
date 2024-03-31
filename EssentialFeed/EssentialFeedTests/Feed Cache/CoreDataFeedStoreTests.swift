22dc0f71a69802bc3eadabe6d826136e3295c9c1
GeorgeSolorio <Georgemsolorio@gmail.com>
2024-03-30 17:30:40 -0700

    Replace `try!` with throwing test methods It's best to throw errors in the test methods, so you get a failing test rather than a crash

--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -9,83 +9,83 @@ import XCTest
 import EssentialFeed
 
 class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
-    func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = makeSUT()
+    func test_retrieve_deliversEmptyOnEmptyCache() throws {
+        let sut = try makeSUT()
         
         assertRetrieveDeliversEmptyOnEmptyCache(on: sut)
     }
     
-    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        let sut = makeSUT()
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() throws {
+        let sut = try makeSUT()
         
         assertRetrieveHasNoSideEffectsOnEmptyCache(on: sut)
     }
     
-    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
-        let sut = makeSUT()
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() throws {
+        let sut = try makeSUT()
         
         assertRetrieveDeliversFoundValuesOnNonEmptyCache(on: sut)
     }
     
-    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
-        let sut = makeSUT()
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() throws {
+        let sut = try makeSUT()
         
         assertRetrieveHasNoSideEffectsOnNonEmptyCache(on: sut)
     }
     
-    func test_insert_deliversNoErrorOnEmptyCache() {
-        let sut = makeSUT()
+    func test_insert_deliversNoErrorOnEmptyCache() throws {
+        let sut = try makeSUT()
         
         assertInsertDeliversNoErrorOnEmptyCache(on: sut)
     }
     
-    func test_insert_deliversNoErrorOnNonEmptyCache() {
-        let sut = makeSUT()
+    func test_insert_deliversNoErrorOnNonEmptyCache() throws {
+        let sut = try makeSUT()
         
         assertInsertDeliversNoErrorOnNonEmptyCache(on: sut)
     }
     
-    func test_insert_overridesPreviouslyInsertedCacheValues() {
-        let sut = makeSUT()
+    func test_insert_overridesPreviouslyInsertedCacheValues() throws {
+        let sut = try makeSUT()
         
         assertInsertOverridesPreviouslyInsertedCacheValues(on: sut)
     }
     
-    func test_delete_deliversNoErrorOnEmptyCache() {
-        let sut = makeSUT()
+    func test_delete_deliversNoErrorOnEmptyCache() throws {
+        let sut = try makeSUT()
         
         assertDeleteDeliversNoErrorOnEmptyCache(on: sut)
     }
     
-    func test_delete_deliversNoErrorOnNonEmptyCache() {
-        let sut = makeSUT()
+    func test_delete_deliversNoErrorOnNonEmptyCache() throws {
+        let sut = try makeSUT()
         
         assertDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
     }
     
-    func test_delete_hasNoSideEffectsOnEmptyCache() {
-        let sut = makeSUT()
+    func test_delete_hasNoSideEffectsOnEmptyCache() throws {
+        let sut = try makeSUT()
         
         assertDeleteHasNoSideEffectsOnEmptyCache(on: sut)
     }
     
-    func test_delete_emptiesPreviosulyInsertedCache() {
-        let sut = makeSUT()
+    func test_delete_emptiesPreviosulyInsertedCache() throws {
+        let sut = try makeSUT()
         
         assertDeleteEmptiesPreviosulyInsertedCache(on: sut)
     }
     
-    func test_storeSideEffects_runSerially() {
-        let sut = makeSUT()
+    func test_storeSideEffects_runSerially() throws {
+        let sut = try makeSUT()
         
         assertStoreSideEffectsRunSerially(on: sut)
     }
     
     // MARK: Helper methods
-    private func makeSUT() -> FeedStore {
+    private func makeSUT() throws -> FeedStore {
         let storeURL = URL(fileURLWithPath: "/dev/null")
         let bundle = Bundle(for: CoreDataFeedStore.self)
-        let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: bundle)
+        let sut = try CoreDataFeedStore(storeURL: storeURL, bundle: bundle)
         trackForMemoryLeak(sut)
         return sut
     }
