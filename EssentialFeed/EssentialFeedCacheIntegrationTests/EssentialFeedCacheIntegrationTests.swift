d48920f095aeccb9a57cf5f96e303e8ba2713d21
GeorgeSolorio <Georgemsolorio@gmail.com>
2024-03-30 20:27:30 -0700

    Add `EssentialFeedCacheIntegrationTests` to seperate the potentially-slower cache integration tests from fast unit/isolated tests

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by George Solorio on 3/30/24.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
