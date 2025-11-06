d4cee12c1efcd8af8bac8469805d5203f774882b
George Michael Solorio <Georgemsolorio@gmail.com>
2025-11-06 06:44:00 -0800

    Add memory leak detection

--- EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -29,13 +29,13 @@ internal final class FeedItemsMapper {
             )
         }
     }
-
-    internal static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == OK_200 else {
-            throw RemoteFeedLoader.Error.invalidData
+    
+    internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+        guard response.statusCode == OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            return .failure(.invalidData)
         }
         
-        let items = try JSONDecoder().decode(Root.self, from: data).items
-        return items.map { $0.item }
+        let items = root.items.map { $0.item }
+        return .success(items)
     }
 }
